a8cdf3e349caa67ec819324e43f003f82b058518
Sergey Kirgizov <kerzolster@gmail.com>
2016-11-14 08:35:39 +0100

    test.R becomes more and more readable... I think so.

--- test.R
+++ test.R
@@ -2,22 +2,31 @@
 N = 5
 source('q-loup-1.R') # states are cooridnates
 
-## random learning
-chat.move <- chat.move.random; loup.move <- loup.move.Q;  PROBABILITY.OF.RANDOM.ACTION = 0.3; main()
-
-
-## totally random
+## totally random movements, without any learning
 chat.move <- chat.move.random; loup.move <- loup.move.random; PROBABILITY.OF.RANDOM.ACTION = 0; main()
 
 
-## without learning
+## loup moves sometimes random, sometimes not. With learning
+chat.move <- chat.move.random; loup.move <- loup.move.Q;  PROBABILITY.OF.RANDOM.ACTION = 0.3; main()
+
+## loup moves only using Q-function. With learning
 chat.move <- chat.move.random; loup.move <- loup.move.Q; PROBABILITY.OF.RANDOM.ACTION = 0; main()
 
-## vs user
+## User controls chat
 chat.move <- chat.move.user; loup.move <- loup.move.Q; PROBABILITY.OF.RANDOM.ACTION = 0; main()
 
 
 ## cyclic learning...
+## chat moves cyclically
+##
+##  +-----+   +-----+   +-----+   +-----+   +-----+ 
+## 5|   @ |  5|     |  5|     |  5|     |  5|   @ | 
+## 4|     |  4|    @|  4|     |  4|  @  |  4|     |  +-+-+----+
+## 3|     |  3|     |  3|   @ |  3|     |  3|     |  |e|t|c...|
+## 2|     |  2|     |  2|     |  2|     |  2|     |  +-+-+-----
+## 1|     |  1|     |  1|     |  1|     |  1|     | 
+##  +-----+   +-----+   +-----+   +-----+   +-----+ 
+##   12345     12345     12345     12345     12345
 chat <- c(4,5);
 cycl <- list(
     c(1,-1),
@@ -31,16 +40,14 @@ chat.move.cyclic <- function () {
     if (GI > length(cycl)) GI <<- 1
     return (cycl[[GI]])
 }
-## with random actions
+## cyclic chat with sometimes random actions of loup
 chat.move <- chat.move.cyclic; loup.move <- loup.move.Q;  PROBABILITY.OF.RANDOM.ACTION = 0.2; main()
 
 
-## cyclic without random actions
+## cyclic chat without random actions of loup
 chat <- c(4,5)
 chat.move <- chat.move.cyclic; loup.move <- loup.move.Q;  PROBABILITY.OF.RANDOM.ACTION = 0; main()
 
-## user control
+## user controls chat
 chat <- c(4,5)
 chat.move <- chat.move.user; loup.move <- loup.move.Q;  PROBABILITY.OF.RANDOM.ACTION = 0.2; main()
-
-
